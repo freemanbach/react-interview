a452204335a130b2b5c270560610fae0c7e1c50e
freeman lo <flo@radford.edu>
2022-05-02 19:46:01 -0400

    added data

--- db/interview.sql
+++ db/interview.sql
@@ -1,21 +1,26 @@
 
+
+DROP DATABASE IF EXISTS interview;
 CREATE DATABASE IF NOT EXISTS interview;
 use interview;
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES,NO_AUTO_VALUE_ON_ZERO';
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
+SET AUTOCOMMIT=0;
 SET time_zone = '-04:00';
-
+START TRANSACTION;
 
 DROP TABLE IF EXISTS `tbl_lang`;
 CREATE TABLE IF NOT EXISTS `tbl_lang` (
           `id` int(10) NOT NULL,
-          `language` VARCHAR(50) NOT NULL,
+          `lang` VARCHAR(50) NOT NULL,
           PRIMARY KEY (`id`)
         ) ENGINE=InnoDB default charset utf8 COLLATE = utf8_general_ci;
+
+LOCK TABLES `tbl_lang` WRITE;
+INSERT INTO tbl_lang (id, lang) VALUES(1,'python'),(2,'java'),(3,'javascript'), (4, 'go'), (5,'rust');
+UNLOCK TABLES;
 
 -- -----------------------------------------------------
 -- Table `tbl_python`
@@ -24,7 +29,7 @@ DROP TABLE IF EXISTS `tbl_python`;
 CREATE TABLE IF NOT EXISTS `tbl_python` (
           `id` int(10) NOT NULL AUTO_INCREMENT,            
           `py_lang_id` int NOT NULL,
-          `diff_level` varchar(10) NOT NULL,
+          `diff_level` ENUM('1', '2', '3', '4', '5') NOT NULL,
           `question` text NOT NULL,
           `answer` text NOT NULL,
           INDEX parent_index (py_lang_id),
@@ -34,6 +39,10 @@ CREATE TABLE IF NOT EXISTS `tbl_python` (
           ON UPDATE CASCADE
         ) ENGINE=InnoDB AUTO_INCREMENT=1 default charset utf8 COLLATE = utf8_general_ci;
 
+LOCK TABLES `tbl_python` WRITE;
+INSERT INTO tbl_python (py_lang_id, diff_level, question, answer) VALUES(1, '1', "How do we store values ?" , "Via a Variable");
+UNLOCK TABLES;
+
 
 -- -----------------------------------------------------
 -- Table `tbl_java`
@@ -42,7 +51,7 @@ DROP TABLE IF EXISTS `tbl_java`;
 CREATE TABLE IF NOT EXISTS `tbl_java` (
           `id` int(10) NOT NULL AUTO_INCREMENT,
           `java_lang_id` int NOT NULL,
-          `diff_level` varchar(10) NOT NULL,
+          `diff_level` ENUM('1', '2', '3', '4', '5') NOT NULL,
           `question` text NOT NULL,
           `answer` text NOT NULL,
           INDEX parent_index (java_lang_id),
@@ -52,6 +61,9 @@ CREATE TABLE IF NOT EXISTS `tbl_java` (
           ON UPDATE CASCADE
         ) ENGINE=InnoDB AUTO_INCREMENT=1 default charset utf8 COLLATE = utf8_general_ci;
 
+LOCK TABLES `tbl_java` WRITE;
+INSERT INTO tbl_java (java_lang_id, diff_level, question, answer) VALUES(2, '1', "How do we store values ?" , "Via a Variable");
+UNLOCK TABLES;
 
 -- -----------------------------------------------------
 -- Table `tbl_javascript`
@@ -60,7 +72,7 @@ DROP TABLE IF EXISTS `tbl_javascript`;
 CREATE TABLE IF NOT EXISTS `tbl_javascript` (
           `id` int(10) NOT NULL AUTO_INCREMENT,
           `js_lang_id` int NOT NULL,
-          `diff_level` varchar(10) NOT NULL,
+          `diff_level` ENUM('1', '2', '3', '4', '5') NOT NULL,
           `question` text NOT NULL,
           `answer` text NOT NULL,
           INDEX parent_index (js_lang_id),
@@ -70,6 +82,9 @@ CREATE TABLE IF NOT EXISTS `tbl_javascript` (
           ON UPDATE CASCADE
         ) ENGINE=InnoDB AUTO_INCREMENT=1 default charset utf8 COLLATE = utf8_general_ci;
 
+LOCK TABLES `tbl_javascript` WRITE;
+INSERT INTO tbl_javascript (js_lang_id, diff_level, question, answer) VALUES(3, '1', "How do we store values ?" , "Via a Variable");
+UNLOCK TABLES;
 
 -- -----------------------------------------------------
 -- Table `tbl_go`
@@ -78,7 +93,7 @@ DROP TABLE IF EXISTS `tbl_go`;
 CREATE TABLE IF NOT EXISTS `tbl_go` (
           `id` int(10) NOT NULL AUTO_INCREMENT,
           `go_lang_id` int NOT NULL,
-          `diff_level` varchar(10) NOT NULL,
+          `diff_level` ENUM('1', '2', '3', '4', '5') NOT NULL,
           `question` text NOT NULL,
           `answer` text NOT NULL,
           INDEX parent_index (go_lang_id),
@@ -88,7 +103,9 @@ CREATE TABLE IF NOT EXISTS `tbl_go` (
           ON UPDATE CASCADE
         ) ENGINE=InnoDB AUTO_INCREMENT=1 default charset utf8 COLLATE = utf8_general_ci;
 
-
+LOCK TABLES `tbl_go` WRITE;
+INSERT INTO tbl_go (go_lang_id, diff_level, question, answer) VALUES(4, '1', "How do we store values ?" , "Via a Variable");
+UNLOCK TABLES;
 
 -- -----------------------------------------------------
 -- Table `tbl_rust`
@@ -97,7 +114,7 @@ DROP TABLE IF EXISTS `tbl_rust`;
 CREATE TABLE IF NOT EXISTS `tbl_rust` (
           `id` int(10) NOT NULL AUTO_INCREMENT,
           `rust_lang_id` int NOT NULL,
-          `diff_level` varchar(10) NOT NULL,
+          `diff_level` ENUM('1', '2', '3', '4', '5') NOT NULL,
           `question` text NOT NULL,
           `answer` text NOT NULL,
           INDEX parent_index (rust_lang_id),
@@ -107,6 +124,10 @@ CREATE TABLE IF NOT EXISTS `tbl_rust` (
           ON UPDATE CASCADE
         ) ENGINE=InnoDB AUTO_INCREMENT=1 default charset utf8 COLLATE = utf8_general_ci;
 
+LOCK TABLES `tbl_rust` WRITE;
+INSERT INTO tbl_rust (rust_lang_id, diff_level, question, answer) VALUES(5, '1', "How do we store values ?" , "Via a Variable");
+UNLOCK TABLES;
+COMMIT;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
